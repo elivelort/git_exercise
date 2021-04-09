4986d7c76cc665d113012cb04f24dccf5ab247bc
Eli <eli.velort@gmail.com>
2021-04-08 20:07:12 -0500

    Create mult and div methods

---
+++ Calculator/src/calculator/AdvancedCalculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class AdvancedCalculator {
+	
+	public static int multiplication(int a, int b) {
+		return a*b;
+	}
+	
+	public static double division(int a, int b) {
+		return b==0 ? 0:a/b;
+	}
+
+}
