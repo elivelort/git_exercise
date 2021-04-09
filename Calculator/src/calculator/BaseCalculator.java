5fdde0153e6ec0b4fc539c5e7b57869a6a9ced79
Eli <eli.velort@gmail.com>
2021-04-08 20:03:48 -0500

    Create sum and substract methods

---
+++ Calculator/src/calculator/BaseCalculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class BaseCalculator {
+	
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public static int substract(int a, int b) {
+		return a-b;
+	}
+	
+	
+}
